dbbf7850ecfd0c3efd6bfd5db87a599169710d0c
Camilo Figueroa <cigotete@gmail.com>
2024-04-11 18:36:07 -0500

    Added main.py file to launch crew process.

---
+++ main.py
@@ -0,0 +1,28 @@
+from crewai import Crew, Process
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+from tasks import tasks
+task_cli_create_dir = tasks.task_cli_create_dir
+task_clone_git_project = tasks.task_clone_git_project
+
+from agents import agents
+agent_cli_manager = agents.agent_cli_manager
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125") #gpt-4-1106-preview
+
+cli_crew = Crew(
+    agents=[
+      agent_cli_manager
+      ],
+    tasks=[
+      task_cli_create_dir,
+      task_clone_git_project
+      ],
+    process=Process.sequential,
+    manager_llm=llm
+)
+
+result = cli_crew.kickoff()
+print(result)
